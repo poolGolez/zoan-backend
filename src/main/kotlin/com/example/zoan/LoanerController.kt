da5075091502c6de7be3ce19a762a9c6f413f9b2
pgolez <paul.golez@codingavenue.com>
2020-04-10 21:44:32 +0800

    Change: Create endpoint to fetch specific loaner using id Description: Show all/specific loaner(s)

--- src/main/kotlin/com/example/zoan/LoanerController.kt
+++ src/main/kotlin/com/example/zoan/LoanerController.kt
@@ -1,7 +1,10 @@
 package com.example.zoan
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/loaners")
@@ -12,6 +15,11 @@ class LoanerController
 
     @GetMapping
     fun list(): Iterable<Loaner> {
-        return repository.findAll();
+        return repository.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun show(@PathVariable id: Long): Any {
+        return repository.findById(id)
     }
 }
