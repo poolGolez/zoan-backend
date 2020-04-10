23b6550fc3c8808328e2ebe60907c109f088ba73
pgolez <paul.golez@codingavenue.com>
2020-04-10 21:21:54 +0800

    Change: Reformat files Ticket #: 1 Description: Loaner model

--- src/main/kotlin/com/example/zoan/Loaner.kt
+++ src/main/kotlin/com/example/zoan/Loaner.kt
@@ -3,32 +3,32 @@ package com.example.zoan
 import javax.persistence.*
 
 @Entity
-@Table(name="loaners")
+@Table(name = "loaners")
 class Loaner() {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="loaners_id_seq")
-    @SequenceGenerator(name="loaners_id_seq", sequenceName = "loaners_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "loaners_id_seq")
+    @SequenceGenerator(name = "loaners_id_seq", sequenceName = "loaners_id_seq")
     var id: Long? = null
         protected set
 
-	var name: String? = null
+    var name: String? = null
 
-	var amountFree: Double = 0.00
-		protected set
+    var amountFree: Double = 0.00
+        protected set
 
-	var amountLoaned: Double = 0.00
-		protected set
+    var amountLoaned: Double = 0.00
+        protected set
 
     var status: LoanerStatus = LoanerStatus.ACTIVE
         protected set
 
-    constructor(capital: Double): this() {
+    constructor(capital: Double) : this() {
         this.amountFree = capital
         this.amountLoaned = 0.00
     }
 
-    constructor(name: String, capital: Double): this(capital) {
+    constructor(name: String, capital: Double) : this(capital) {
         this.name = name
     }
 
@@ -43,13 +43,13 @@ class Loaner() {
     }
 
     fun deactivate() {
-        if(this.status == LoanerStatus.ACTIVE) {
+        if (this.status == LoanerStatus.ACTIVE) {
             this.status = LoanerStatus.INACTIVE
         }
     }
 
     fun activate() {
-        if(this.status == LoanerStatus.INACTIVE) {
+        if (this.status == LoanerStatus.INACTIVE) {
             this.status = LoanerStatus.ACTIVE
         }
     }
