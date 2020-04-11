07d6e053f87c227bae96b21e5ac20ff59909cc45
pgolez <paul.golez@codingavenue.com>
2020-04-12 00:02:57 +0800

    Change: Create borrower model Ticket #: 11 Description: Borrower model

---
+++ src/main/kotlin/com/example/zoan/domain/borrower/Borrower.kt
@@ -0,0 +1,21 @@
+package com.example.zoan.domain.borrower
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "borrowers")
+class Borrower {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "borrowers_id_seq")
+    @SequenceGenerator(name = "borrowers_id_seq", sequenceName = "borrowers_id_seq")
+    var id: Long? = null
+
+    var name: String? = null
+
+    @Enumerated(EnumType.STRING)
+    var status: BorrowerStatus = BorrowerStatus.ACTIVE
+
+    enum class BorrowerStatus {
+        ACTIVE, INACTIVE
+    }
+}
