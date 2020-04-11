feabc1a86891691c497e8cd801409e0facb90e9b
pgolez <paul.golez@codingavenue.com>
2020-04-11 16:36:44 +0800

    Change: Create endpoint to get specific fund Ticket #: 7 Description: Show all/specific fund(s)

--- src/main/kotlin/com/example/zoan/FundController.kt
+++ src/main/kotlin/com/example/zoan/FundController.kt
@@ -1,9 +1,13 @@
 package com.example.zoan
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 @RestController
@@ -17,6 +21,12 @@ class FundController {
     fun list(): List<FundDTO> {
         val funds = repository.findAll()
         return funds.map { FundDTO(it) }
+    }
+
+    @GetMapping("/{id}")
+    fun show(@PathVariable id: Long): FundDTO {
+        val fund = repository.findByIdOrNull(id) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+        return FundDTO(fund)
     }
 }
 
