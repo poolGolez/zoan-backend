82b586b47cf0cb0595788bd2c6f875bc43c44ddc
pgolez <paul.golez@codingavenue.com>
2020-04-12 14:40:07 +0800

    Change: Add endpoint to show specific loan Ticket #: 17 Description: Show all/specific loan(s)

--- src/main/kotlin/com/example/zoan/http/loan/LoanController.kt
+++ src/main/kotlin/com/example/zoan/http/loan/LoanController.kt
@@ -2,9 +2,13 @@ package com.example.zoan.http.loan
 
 import com.example.zoan.domain.loan.LoanRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/loans")
@@ -16,6 +20,13 @@ class LoanController {
     @GetMapping
     fun list(): List<LoanDto> {
         return repository.findAll().map { loan -> LoanDto(loan) }
+    }
+
+    @GetMapping("{id}")
+    fun show(@PathVariable id: Long): LoanDto {
+        val loan = repository.findByIdOrNull(id)
+                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+        return LoanDto(loan)
     }
 
 }
