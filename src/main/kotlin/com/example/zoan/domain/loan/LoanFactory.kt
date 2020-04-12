985f258e7abe37d5ee44bf2f7db1e7d6b5d12218
pgolez <paul.golez@codingavenue.com>
2020-04-12 16:16:23 +0800

    Change: Assign funds if specified in request Ticket #: 18 Description: Create new loan model

--- src/main/kotlin/com/example/zoan/domain/loan/LoanFactory.kt
+++ src/main/kotlin/com/example/zoan/domain/loan/LoanFactory.kt
@@ -1,6 +1,7 @@
 package com.example.zoan.domain.loan
 
 import com.example.zoan.domain.borrower.BorrowerRepository
+import com.example.zoan.domain.fund.FundRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
@@ -11,12 +12,23 @@ class LoanFactory {
     @Autowired
     lateinit var borrowerRepository: BorrowerRepository
 
+    @Autowired
+    lateinit var fundRepository: FundRepository
+
     fun createLoan(params: CreateLoanParams): Loan {
         val borrower = borrowerRepository.findByIdOrNull(params.borrowerId)
-                ?: throw Exception("Unknow borrower")
+                ?: throw Exception("Unknown borrower")
         val loan = Loan(params.amount, borrower)
+
         loan.monthlyInterest = params.monthlyInterest
         loan.installmentCount = params.installmentCount
+
+        params.fundId?.let { fundId ->
+            val fund = fundRepository.findByIdOrNull(fundId)
+                    ?: throw Exception("Unknown funds")
+            loan.allocate(fund)
+        }
+
         return loan
     }
 
