394f0889bfcee8e30b6d7c412e564cbca174eb73
pgolez <paul.golez@codingavenue.com>
2020-04-14 22:06:07 +0800

    Change: Cleanup other repositories Ticket #: 23 Description: List payment schedule(s) for loan

--- src/main/kotlin/com/example/zoan/domain/fund/FundRepository.kt
+++ src/main/kotlin/com/example/zoan/domain/fund/FundRepository.kt
@@ -1,9 +1,7 @@
 package com.example.zoan.domain.fund
 
-import com.example.zoan.domain.fund.Fund
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FundRepository : CrudRepository<Fund, Long> {
-}
+interface FundRepository : CrudRepository<Fund, Long>
--- src/main/kotlin/com/example/zoan/domain/loaner/LoanerRepository.kt
+++ src/main/kotlin/com/example/zoan/domain/loaner/LoanerRepository.kt
@@ -1,9 +1,7 @@
 package com.example.zoan.domain.loaner
 
-import com.example.zoan.domain.loaner.Loaner
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface LoanerRepository: CrudRepository<Loaner, Long> {
-}
+interface LoanerRepository : CrudRepository<Loaner, Long>
