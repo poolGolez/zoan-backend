6f2a108c1e9c64eb6daa54c66fce53690edf030e
pgolez <paul.golez@codingavenue.com>
2020-04-14 21:16:05 +0800

    Change: Initial class for payment schedule Ticket #: 22 Description: Payment schedule model

---
+++ src/main/kotlin/com/example/zoan/domain/loan/PaymentSchedule.kt
@@ -0,0 +1,30 @@
+package com.example.zoan.domain.loan
+
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "payment_schedules")
+class PaymentSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_schedules_id_seq")
+    @SequenceGenerator(name = "payment_schedules_id_seq", sequenceName = "payment_schedules_id_seq")
+    var id: Long? = null
+
+    @ManyToOne
+    @JoinColumn(name = "loan_id", nullable = false)
+    lateinit var loan: Loan
+
+    var amount: Double = 0.00
+
+    lateinit var dateDue: Date
+
+    @Enumerated(EnumType.STRING)
+    var status = PaymentScheduleStatus.PENDING
+        protected set
+
+    enum class PaymentScheduleStatus {
+        PENDING, COVERED
+    }
+}
