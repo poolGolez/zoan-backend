b055c2e49fd012d11ffde7d3d3e8f2665a647149
pgolez <paul.golez@codingavenue.com>
2020-04-15 22:09:02 +0800

    Change: Add generation of payment schedules when activating loan Ticket #: 24 Description: Generate payment schedule upon activation of loan

--- src/main/kotlin/com/example/zoan/domain/loan/LoanService.kt
+++ src/main/kotlin/com/example/zoan/domain/loan/LoanService.kt
@@ -3,15 +3,23 @@ package com.example.zoan.domain.loan
 import com.example.zoan.ZoanException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class LoanService {
 
     @Autowired
     lateinit var loanFactory: LoanFactory
 
     @Autowired
+    lateinit var paymentSchedFactory: PaymentScheduleFactory
+
+    @Autowired
     lateinit var loanRepository: LoanRepository
+
+    @Autowired
+    lateinit var paymentSchedRepo: PaymentScheduleRepository
 
     fun createLoan(params: CreateLoanParams): Loan {
         try {
@@ -21,12 +29,14 @@ class LoanService {
         } catch (exception: ZoanException) {
             throw LoanCreationException(exception)
         }
-
     }
 
     fun activateLoan(loan: Loan): Loan {
         loan.activate()
+        val paymentSchedules = paymentSchedFactory.createPaymentSchedules(loan)
+
         loanRepository.save(loan)
+        paymentSchedRepo.saveAll(paymentSchedules)
         return loan
     }
 }
