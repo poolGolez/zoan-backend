b7c957d06695279918e179745d37b5a57d30bbc4
pgolez <paul.golez@codingavenue.com>
2020-04-20 20:56:21 +0800

    Change: Create initial model for Payment Ticket #: 25 Description: Payment and PaymentAllotment models

---
+++ src/main/kotlin/com/example/zoan/domain/payment/Payment.kt
@@ -0,0 +1,24 @@
+package com.example.zoan.domain.payment
+
+import com.example.zoan.domain.borrower.Borrower
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+@Table(name = "payments")
+class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payments_id_seq")
+    @SequenceGenerator(name = "payments_id_seq", sequenceName = "payments_id_seq")
+    var id: Long? = null
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id")
+    lateinit var payer: Borrower
+
+    var amount: Double = 0.00
+
+    lateinit var transactionDate: LocalDate
+
+    var dateCreated = LocalDate.now()
+}
