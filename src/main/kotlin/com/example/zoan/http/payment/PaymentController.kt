ab06a18b21d5d4315c4bcda52a402df90df50cb8
pgolez <paul.golez@codingavenue.com>
2020-04-20 21:26:45 +0800

    Change: Create endpoint to fetch specific payment Ticket #: 26 Description: List specific/all payment(s)

--- src/main/kotlin/com/example/zoan/http/payment/PaymentController.kt
+++ src/main/kotlin/com/example/zoan/http/payment/PaymentController.kt
@@ -2,9 +2,13 @@ package com.example.zoan.http.payment
 
 import com.example.zoan.domain.payment.PaymentRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/payments")
@@ -17,5 +21,12 @@ class PaymentController {
     fun list(): List<PaymentDto> {
         val payments = repository.findAll()
         return payments.map { payment -> PaymentDto(payment) }
+    }
+
+    @GetMapping("/{id}")
+    fun show(@PathVariable id: Long): PaymentDto {
+        val payment = repository.findByIdOrNull(id)
+                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+        return PaymentDto(payment)
     }
 }
