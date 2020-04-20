d50cda5fb70739b0591a28d6370bc7e579811e41
pgolez <paul.golez@codingavenue.com>
2020-04-20 21:24:18 +0800

    Change: Create repository for paymnets Ticket #: 26 Description: List specific/all payment(s)

---
+++ src/main/kotlin/com/example/zoan/domain/payment/PaymentRepository.kt
@@ -0,0 +1,7 @@
+package com.example.zoan.domain.payment
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PaymentRepository : CrudRepository<Payment, Long>
