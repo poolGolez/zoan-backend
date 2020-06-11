88b5870f7ba555e758a6ea23a28bd30ede5f0253
pgolez <paul.golez@codingavenue.com>
2020-06-11 22:27:53 +0800

    Change: Create method to fetch transactions by loaners Ticket #: 28 Description: Create new equity transaction

--- src/main/kotlin/com/example/zoan/domain/equity/TransactionRepository.kt
+++ src/main/kotlin/com/example/zoan/domain/equity/TransactionRepository.kt
@@ -1,7 +1,10 @@
 package com.example.zoan.domain.equity
 
+import com.example.zoan.domain.loaner.Loaner
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TransactionRepository : CrudRepository<Transaction, Long>
+interface TransactionRepository : CrudRepository<Transaction, Long> {
+    fun findByLoaner(loaner: Loaner): List<Transaction>
+}
