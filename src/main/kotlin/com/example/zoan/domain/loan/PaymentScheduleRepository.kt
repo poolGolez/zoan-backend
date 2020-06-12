7b7e3db12692ca8babc15fbfb48ccb2fed92732f
pgolez <paul.golez@codingavenue.com>
2020-06-12 13:56:09 +0800

    Change: Create new method to find schedules within date range Ticket #: 29 Description: Retrieve payment schedules

--- src/main/kotlin/com/example/zoan/domain/loan/PaymentScheduleRepository.kt
+++ src/main/kotlin/com/example/zoan/domain/loan/PaymentScheduleRepository.kt
@@ -2,9 +2,16 @@ package com.example.zoan.domain.loan
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Repository
 interface PaymentScheduleRepository : CrudRepository<PaymentSchedule, Long> {
     fun findByLoan(loan: Loan): List<PaymentSchedule>
     fun findByIdAndLoan(id: Long, loan: Loan): PaymentSchedule?
+    fun findByDateDueBetween(from: LocalDate, to: LocalDate): List<PaymentSchedule>
 }
+
+data class PaymentScheduleQueryParams(
+        val dateFrom: LocalDate,
+        val dateTo: LocalDate
+)
